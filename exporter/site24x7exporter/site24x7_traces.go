f9f7ffd704d58d1f808eada6a866e985146ee528
Shrinivasan S S <shrinivasan.s@zohocorp.com>
2021-11-26 13:44:19 +0530

    traces export updated.

--- exporter/site24x7exporter/site24x7_traces.go
+++ exporter/site24x7exporter/site24x7_traces.go
@@ -28,19 +28,41 @@ import (
 )
 
 
-func (e *site24x7exporter) CreateTelemetrySpan(span pdata.Span, resourceAttr map[string]interface{}, serviceName string) (TelemetrySpan) {
+func (e *site24x7exporter) CreateTelemetrySpan(span pdata.Span, 
+	resourceAttr map[string]interface{}, 
+	serviceName string,
+	instLibrary string, 
+	instLibraryVersion string,
+	telSDKLang string, 
+	telSDKName string) (TelemetrySpan) {
 	
 	spanAttr := span.Attributes().AsRaw()
 	startTime := (span.StartTimestamp().AsTime().UnixNano()) // int64(time.Millisecond))
 	endTime := (span.EndTimestamp().AsTime().UnixNano()) // int64(time.Millisecond))
 	spanEvts := span.Events()
 	telEvents := make([]TelemetrySpanEvent,0,spanEvts.Len())
+	exceptionMessages := make([]string, 0, spanEvts.Len())
+	exceptionStackTraces := make([]string, 0, spanEvts.Len())
+	exceptionTypes := make([]string, 0, spanEvts.Len())
 	for i := 0; i < spanEvts.Len(); i++ {
 		spanEvt := spanEvts.At(i)
 		telEvt := TelemetrySpanEvent{
 			Timestamp: (spanEvt.Timestamp().AsTime().UnixNano() / int64(time.Millisecond)),
 			Name: spanEvt.Name(),
 			EventAttributes: spanEvt.Attributes().AsRaw(),
+		}
+		exMsg,found := telEvt.EventAttributes["exception.message"]
+		if found {
+			exceptionMessages = append(exceptionMessages, exMsg.(string))
+		}
+
+		exST, found := telEvt.EventAttributes["exception.stacktrace"]
+		if found {
+			exceptionStackTraces = append(exceptionStackTraces, exST.(string))
+		}
+		exType, found := telEvt.EventAttributes["exception.type"]
+		if found {
+			exceptionTypes = append(exceptionTypes, exType.(string))
 		}
 		telEvents = append(telEvents, telEvt)
 	}
@@ -55,6 +77,53 @@ func (e *site24x7exporter) CreateTelemetrySpan(span pdata.Span, resourceAttr map
 		telLinks = append(telLinks, telLink)
 	}
 	spanState := span.Status()
+
+	// Host attributes
+	var hostIp, hostName, hostPort string
+	if attrval,found := spanAttr["net.peer.ip"]; found {
+		hostIp = attrval.(string)
+	}
+	if attrval,found := spanAttr["net.peer.name"]; found {
+		hostName = attrval.(string)
+	}
+	if attrval,found := spanAttr["net.peer.port"]; found {
+		hostPort = attrval.(string)
+	}
+	// Thread attributes
+	var threadid, threadname string
+	if attrval,found := spanAttr["thread.id"]; found {
+		threadid = attrval.(string)
+	}
+	if attrval,found := spanAttr["thread.name"]; found {
+		threadname = attrval.(string)
+	}
+	// DB attributes
+	var dbsystem, dbstmt, dbname, dbconnstr string
+	if attrval,found := spanAttr["db.system"]; found {
+		dbsystem = attrval.(string)
+	}
+	if attrval,found := spanAttr["db.statement"]; found {
+		dbstmt = attrval.(string)
+	}
+	if attrval,found := spanAttr["db.name"]; found {
+		dbname = attrval.(string)
+	}
+	if attrval,found := spanAttr["db.connection_string"]; found {
+		dbconnstr = attrval.(string)
+	}
+	// Http attributes
+	var httpurl, httpmethod, httpstatus string
+	if attrval,found := spanAttr["http.url"]; found {
+		httpurl = attrval.(string)
+	}
+	if attrval,found := spanAttr["http.method"]; found {
+		httpmethod = attrval.(string)
+	}
+	if attrval,found := spanAttr["http.status_code"]; found {
+		httpstatus = attrval.(string)
+	}
+
+	isRoot := span.ParentSpanID().IsEmpty()
 	
 	tspan := TelemetrySpan{
 		SpanId: span.SpanID().HexString(),
@@ -76,6 +145,34 @@ func (e *site24x7exporter) CreateTelemetrySpan(span pdata.Span, resourceAttr map
 		DroppedLinksCount: span.DroppedLinksCount(),
 		DroppedEventsCount: span.DroppedEventsCount(),
 		TraceState: string(span.TraceState()),
+
+		ExceptionMessage: exceptionMessages,
+		ExceptionStackTrace: exceptionStackTraces,
+		ExceptionType: exceptionTypes,
+
+		InstrumentationLibrary: instLibrary,
+		InstrumentationLibraryVersion: instLibraryVersion,
+		TelemetrySDKLanguage: telSDKLang,
+		TelemetrySDKName: telSDKName,
+
+		HostIP: hostIp,
+		HostName: hostName,
+		HostPort: hostPort,
+
+		ThreadId: threadid,
+		ThreadName: threadname,
+
+		DbSystem: dbsystem,
+		DbStatement: dbstmt,
+		DbName: dbname,
+		DbConnStr: dbconnstr,
+
+		HttpUrl: httpurl,
+		HttpMethod: httpmethod,
+		HttpStatusCode: httpstatus,
+
+		IsRoot: isRoot,
+
 	}
 	return tspan;
 }
@@ -101,13 +198,21 @@ func (e *site24x7exporter) ConsumeTraces(_ context.Context, td pdata.Traces) err
 		resource := rspans.Resource()
 		resourceAttr := resource.Attributes().AsRaw()
 		serviceName := resourceAttr["service.name"].(string)
+		telSDKName := resourceAttr["telemetry.sdk.name"].(string)
+		telSDKLang := resourceAttr["telemetry.sdk.language"].(string)
 		instSpans := rspans.InstrumentationLibrarySpans()
+
 		for j := 0; j < instSpans.Len(); j++ {
 			ispans := instSpans.At(j)
+			instLibName := ispans.InstrumentationLibrary().Name()
+			instLibVer := ispans.InstrumentationLibrary().Version()
 			ispanItems := ispans.Spans()
 			for k := 0; k < ispanItems.Len(); k++ {
 				span := ispanItems.At(k)
-				s247span := e.CreateTelemetrySpan(span, resourceAttr, serviceName)
+				s247span := e.CreateTelemetrySpan(span, resourceAttr, 
+					serviceName, 
+					instLibName, instLibVer,
+					telSDKLang, telSDKName)
 				spanList = append(spanList, s247span)
 			}
 		}

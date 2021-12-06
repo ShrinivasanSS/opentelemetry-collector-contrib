fc846118e4aeb4141fe4cc6ed2d9961307a2aaa4
shrinivasan <shrinivasan.s@zohocorp.com>
2021-12-06 15:39:10 +0530

    data format change.

--- exporter/site24x7exporter/site24x7_traces.go
+++ exporter/site24x7exporter/site24x7_traces.go
@@ -27,41 +27,40 @@ import (
 	"go.opentelemetry.io/collector/model/pdata"
 )
 
-
-func (e *site24x7exporter) CreateTelemetrySpan(span pdata.Span, 
-	resourceAttr map[string]interface{}, 
+func (e *site24x7exporter) CreateTelemetrySpan(span pdata.Span,
+	resourceAttr map[string]interface{},
 	serviceName string,
-	instLibrary string, 
+	instLibrary string,
 	instLibraryVersion string,
-	telSDKLang string, 
-	telSDKName string) (TelemetrySpan) {
-	
+	telSDKLang string,
+	telSDKName string) TelemetrySpan {
+
 	spanAttr := span.Attributes().AsRaw()
 	startTime := (span.StartTimestamp().AsTime().UnixNano()) // int64(time.Millisecond))
-	endTime := (span.EndTimestamp().AsTime().UnixNano()) // int64(time.Millisecond))
+	endTime := (span.EndTimestamp().AsTime().UnixNano())     // int64(time.Millisecond))
 	spanEvts := span.Events()
-	telEvents := make([]TelemetrySpanEvent,0,spanEvts.Len())
+	telEvents := make([]TelemetrySpanEvent, 0, spanEvts.Len())
 	exceptionMessages := make([]string, 0, spanEvts.Len())
 	exceptionStackTraces := make([]string, 0, spanEvts.Len())
 	exceptionTypes := make([]string, 0, spanEvts.Len())
 	for i := 0; i < spanEvts.Len(); i++ {
 		spanEvt := spanEvts.At(i)
 		telEvt := TelemetrySpanEvent{
-			Timestamp: (spanEvt.Timestamp().AsTime().UnixNano() / int64(time.Millisecond)),
-			Name: spanEvt.Name(),
+			Timestamp:       (spanEvt.Timestamp().AsTime().UnixNano() / int64(time.Millisecond)),
+			Name:            spanEvt.Name(),
 			EventAttributes: spanEvt.Attributes().AsRaw(),
 		}
 		if telEvt.EventAttributes != nil {
 
-			if exMsg,found := telEvt.EventAttributes["exception.message"] ; found {
+			if exMsg, found := telEvt.EventAttributes["exception.message"]; found {
 				exceptionMessages = append(exceptionMessages, exMsg.(string))
 			}
-			
-			if exST, found := telEvt.EventAttributes["exception.stacktrace"] ; found {
+
+			if exST, found := telEvt.EventAttributes["exception.stacktrace"]; found {
 				exceptionStackTraces = append(exceptionStackTraces, exST.(string))
 			}
-			
-			if exType, found := telEvt.EventAttributes["exception.type"] ; found {
+
+			if exType, found := telEvt.EventAttributes["exception.type"]; found {
 				exceptionTypes = append(exceptionTypes, exType.(string))
 			}
 		}
@@ -72,7 +71,7 @@ func (e *site24x7exporter) CreateTelemetrySpan(span pdata.Span,
 	for i := 0; i < spanLinks.Len(); i++ {
 		spanLink := spanLinks.At(i)
 		telLink := TelemetrySpanLink{
-			LinkSpanID: spanLink.SpanID().HexString(),
+			LinkSpanID:  spanLink.SpanID().HexString(),
 			LinkTraceID: spanLink.TraceID().HexString(),
 		}
 		telLinks = append(telLinks, telLink)
@@ -88,128 +87,131 @@ func (e *site24x7exporter) CreateTelemetrySpan(span pdata.Span,
 	spanKind := "UNSPECIFIED"
 	switch span.Kind() {
 	case pdata.SpanKindInternal:
-		spanKind="INTERNAL"
+		spanKind = "INTERNAL"
 	case pdata.SpanKindServer:
-		spanKind="SERVER"
+		spanKind = "SERVER"
 	case pdata.SpanKindClient:
-		spanKind="CLIENT"
+		spanKind = "CLIENT"
 	case pdata.SpanKindProducer:
-		spanKind="PRODUCER"
+		spanKind = "PRODUCER"
 	case pdata.SpanKindConsumer:
-		spanKind="CONSUMER"
+		spanKind = "CONSUMER"
 	}
 
 	// Host attributes
 	var hostIp, hostName, threadname string
-	var hostPort,threadid int64
-	if attrval,found := spanAttr["net.peer.ip"]; found {
+	var hostPort, threadid int64
+	if attrval, found := spanAttr["net.peer.ip"]; found {
 		hostIp = attrval.(string)
 	}
-	if attrval,found := spanAttr["net.peer.name"]; found {
+	if attrval, found := spanAttr["net.peer.name"]; found {
 		hostName = attrval.(string)
 	}
-	if attrval,found := spanAttr["net.peer.port"]; found {
+	if attrval, found := spanAttr["net.peer.port"]; found {
 		hostPort = attrval.(int64)
 	}
 	// Thread attributes
-	if attrval,found := spanAttr["thread.id"]; found {
+	if attrval, found := spanAttr["thread.id"]; found {
 		threadid = attrval.(int64)
 	}
-	if attrval,found := spanAttr["thread.name"]; found {
+	if attrval, found := spanAttr["thread.name"]; found {
 		threadname = attrval.(string)
 	}
 	// DB attributes
 	var dbsystem, dbstmt, dbname, dbconnstr string
-	if attrval,found := spanAttr["db.system"]; found {
+	if attrval, found := spanAttr["db.system"]; found {
 		dbsystem = attrval.(string)
 	}
-	if attrval,found := spanAttr["db.statement"]; found {
+	if attrval, found := spanAttr["db.statement"]; found {
 		dbstmt = attrval.(string)
 	}
-	if attrval,found := spanAttr["db.name"]; found {
+	if attrval, found := spanAttr["db.name"]; found {
 		dbname = attrval.(string)
 	}
-	if attrval,found := spanAttr["db.connection_string"]; found {
+	if attrval, found := spanAttr["db.connection_string"]; found {
 		dbconnstr = attrval.(string)
 	}
 	// Http attributes
 	var httpurl, httpmethod string
 	var httpstatus int64
-	if attrval,found := spanAttr["http.url"]; found {
+	if attrval, found := spanAttr["http.url"]; found {
 		httpurl = attrval.(string)
 	}
-	if attrval,found := spanAttr["http.method"]; found {
+	if attrval, found := spanAttr["http.method"]; found {
 		httpmethod = attrval.(string)
 	}
-	if attrval,found := spanAttr["http.status_code"]; found {
+	if attrval, found := spanAttr["http.status_code"]; found {
 		httpstatus = attrval.(int64)
 	}
 
 	isRoot := span.ParentSpanID().IsEmpty()
-	telemetryParams := make([]TelemetryCustomParam, 0, len(spanAttr) )
-	for k,v := range spanAttr {
+	telemetryParams := make([]TelemetryCustomParam, 0, len(spanAttr))
+	for k, v := range spanAttr {
 		telemetrycustomParam := TelemetryCustomParam{
-			Key: k,
+			Key:   k,
 			Value: v,
 		}
 		telemetryParams = append(telemetryParams, telemetrycustomParam)
 	}
-	
+
+	spanid := span.SpanID().HexString()
+	traceId := span.TraceID().HexString()
+	parentspanId := span.ParentSpanID().HexString()
+	spanName := span.Name()
+
 	tspan := TelemetrySpan{
-		SpanId: span.SpanID().HexString(),
-		TraceId: span.TraceID().HexString(),
-		ParentSpanId: span.ParentSpanID().HexString(),
-		Name: span.Name(),
-		Kind: spanKind,
-		StartTime: startTime,
-		EndTime: endTime,
-		Duration: float64(endTime - startTime) / float64(time.Millisecond),
-		ServiceName: serviceName,
-		resourceAttributes: resourceAttr,
-		spanAttributes: spanAttr,
-		traceState: string(span.TraceState()),
-		spanEvents: telEvents,
-		spanLinks: telLinks,
-		statusCode: spanState.Code().String(),
-		statusMsg: spanState.Message(),
+		SpanId:                 spanid,
+		TraceId:                traceId,
+		ParentSpanId:           parentspanId,
+		Name:                   spanName,
+		Kind:                   spanKind,
+		StartTime:              startTime,
+		EndTime:                endTime,
+		Duration:               float64(endTime-startTime) / float64(time.Millisecond),
+		ServiceName:            serviceName,
+		resourceAttributes:     resourceAttr,
+		spanAttributes:         spanAttr,
+		traceState:             string(span.TraceState()),
+		spanEvents:             telEvents,
+		spanLinks:              telLinks,
+		statusCode:             spanState.Code().String(),
+		statusMsg:              spanState.Message(),
 		droppedAttributesCount: span.DroppedAttributesCount(),
-		droppedLinksCount: span.DroppedLinksCount(),
-		droppedEventsCount: span.DroppedEventsCount(),
+		droppedLinksCount:      span.DroppedLinksCount(),
+		droppedEventsCount:     span.DroppedEventsCount(),
 
-		ExceptionMessage: exceptionMessages,
+		ExceptionMessage:    exceptionMessages,
 		ExceptionStackTrace: exceptionStackTraces,
-		ExceptionType: exceptionTypes,
+		ExceptionType:       exceptionTypes,
 
-		InstrumentationLibrary: instLibrary,
+		InstrumentationLibrary:        instLibrary,
 		InstrumentationLibraryVersion: instLibraryVersion,
-		TelemetrySDKLanguage: telSDKLang,
-		TelemetrySDKName: telSDKName,
+		TelemetrySDKLanguage:          telSDKLang,
+		TelemetrySDKName:              telSDKName,
 
-		HostIP: hostIp,
+		HostIP:   hostIp,
 		HostName: hostName,
 		HostPort: hostPort,
 
-		ThreadId: threadid,
+		ThreadId:   threadid,
 		ThreadName: threadname,
 
-		DbSystem: dbsystem,
+		DbSystem:    dbsystem,
 		DbStatement: dbstmt,
-		DbName: dbname,
-		DbConnStr: dbconnstr,
+		DbName:      dbname,
+		DbConnStr:   dbconnstr,
 
-		HttpUrl: httpurl,
-		HttpMethod: httpmethod,
+		HttpUrl:        httpurl,
+		HttpMethod:     httpmethod,
 		HttpStatusCode: httpstatus,
 
-		IsRoot: isRoot,
+		IsRoot:   isRoot,
 		HasError: hasError,
 
 		CustomParams: telemetryParams,
-
 	}
-	return tspan;
+	return tspan
 }
-
 
 func (e *site24x7exporter) ConsumeTraces(_ context.Context, td pdata.Traces) error {
 	/*buf, err := tracesMarshaler.MarshalTraces(td)
@@ -232,13 +234,13 @@ func (e *site24x7exporter) ConsumeTraces(_ context.Context, td pdata.Traces) err
 		resourceAttr := resource.Attributes().AsRaw()
 
 		var serviceName, telSDKLang, telSDKName string
-		if val,found := resourceAttr["service.name"]; found {
+		if val, found := resourceAttr["service.name"]; found {
 			serviceName = val.(string)
 		}
-		if val,found := resourceAttr["telemetry.sdk.name"]; found {
+		if val, found := resourceAttr["telemetry.sdk.name"]; found {
 			telSDKName = val.(string)
 		}
-		if val,found := resourceAttr["telemetry.sdk.language"]; found {
+		if val, found := resourceAttr["telemetry.sdk.language"]; found {
 			telSDKLang = val.(string)
 		}
 
@@ -251,8 +253,8 @@ func (e *site24x7exporter) ConsumeTraces(_ context.Context, td pdata.Traces) err
 			ispanItems := ispans.Spans()
 			for k := 0; k < ispanItems.Len(); k++ {
 				span := ispanItems.At(k)
-				s247span := e.CreateTelemetrySpan(span, resourceAttr, 
-					serviceName, 
+				s247span := e.CreateTelemetrySpan(span, resourceAttr,
+					serviceName,
 					instLibName, instLibVer,
 					telSDKLang, telSDKName)
 				spanList = append(spanList, s247span)
@@ -261,14 +263,15 @@ func (e *site24x7exporter) ConsumeTraces(_ context.Context, td pdata.Traces) err
 	}
 	io.WriteString(e.file, "\nTransformed telemetry data to site24x7 format. \n")
 	buf, err := json.Marshal(spanList)
-	if err != nil{
+	if err != nil {
 		io.WriteString(e.file, "\nError in converting telemetry data. \n")
 		errstr := err.Error()
 		io.WriteString(e.file, errstr)
 		return err
 	}
 	responseBody := bytes.NewBuffer(buf)
-	fmt.Fprint(&urlBuf, e.url, "?license.key=",e.apikey);
+	fmt.Println("Sending to Site24x7: ", responseBody)
+	fmt.Fprint(&urlBuf, e.url, "?license.key=", e.apikey)
 	resp, err := http.Post(urlBuf.String(), "application/json", responseBody)
 	io.WriteString(e.file, "\nPosting telemetry data to url. \n")
 	if err != nil {
